529c6b8617a1813298fb575a1e8f652d58e3cf3a
Freddy L <neo-ciber_94@hotmail.com>
2022-04-03 10:59:55 -0400

    Added template for web_api sample

---
+++ samples/web_api/src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dilib::Container;
+use std::sync::Arc;
+
+pub type SharedContainer = web::Data<Container<'static>>;
+
+#[get("/")]
+async fn hello(container: SharedContainer) -> impl Responder {
+    let hello = container.get_scoped::<String>().unwrap();
+    HttpResponse::Ok().body(hello)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
+
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse::<u16>().ok())
+        .unwrap_or(8080);
+
+    HttpServer::new(|| App::new()
+        .app_data(web::Data::new(create_container()))
+        .service(hello))
+        .bind(("127.0.0.1", port))?
+        .run()
+        .await
+}
+
+fn create_container() -> Container<'static> {
+    let mut container = Container::new();
+    container.add_scoped(|| String::from("Hello"));
+
+    container
+}
