8a6a2f87c6aefbd74f73812af140ae958535081a
Freddy L <neo-ciber_94@hotmail.com>
2022-04-02 21:08:54 -0400

    Added new example

--- samples/hello_world/src/main.rs
+++ samples/hello_world/src/main.rs
@@ -1,3 +1,10 @@
+use dilib::Container;
+
 fn main() {
-    println!("Hello, world!");
+    let mut container = Container::new();
+    container.add_singleton("Hello World".to_owned());
+    container.add_scoped(|| "Hello Rust".to_owned());
+
+    println!("{}", container.get_singleton::<String>().unwrap());
+    println!("{}", container.get_scoped::<String>().unwrap());
 }
