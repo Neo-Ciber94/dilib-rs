ad4ef958d5e12c2aae189cb26365b936910d5467
Freddy L <neo-ciber_94@hotmail.com>
2022-04-14 10:38:19 -0400

    fixed rust version

---
+++ dilib-experimental/src/resolve_call.rs
@@ -0,0 +1,53 @@
+use dilib::Container;
+
+pub trait ResolveCall {
+    type Output;
+    fn resolve_call(&self, container: &Container) -> Self::Output;
+}
+
+pub trait ResolveCallMut {
+    type Output;
+    fn resolve_call_mut(&mut self, container: &Container) -> Self::Output;
+}
+
+#[macro_export]
+macro_rules! impl_resolve_call_fn {
+    ($($t:ident),+) => {
+        impl<Out, $($t),+> ResolveCall for Box<dyn Fn($(&$t),+) -> Out>
+         where $($t: Send + Sync + 'static),+ {
+            type Output = Out;
+            fn resolve_call(&self, container: &Container) -> Self::Output {
+                (self)(
+                    $(container.get::<$t>().unwrap().as_ref()),+
+                )
+            }
+        }
+    };
+}
+
+impl_resolve_call_fn!(A);
+impl_resolve_call_fn!(A, B);
+impl_resolve_call_fn!(A, B, C);
+impl_resolve_call_fn!(A, B, C, D);
+impl_resolve_call_fn!(A, B, C, D, E);
+impl_resolve_call_fn!(A, B, C, D, E, F, G);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
+impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
+// impl<Out, A> ResolveCall for Box<dyn Fn(&A) -> Out>
+// where
+//     A: Send + Sync + 'static,
+// {
+//     type Output = Out;
+//
+//     fn resolve_call(&self, container: &Container) -> Self::Output {
+//         (self)(container.get::<A>().unwrap().as_ref())
+//     }
+// }
