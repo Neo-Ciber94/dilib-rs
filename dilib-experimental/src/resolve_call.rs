5203bb761c918c7764a15ee422d508f36ec39378
Freddy L <neo-ciber_94@hotmail.com>
2022-04-14 15:44:31 -0400

    Updated ci.yml

--- dilib-experimental/src/resolve_call.rs
+++ dilib-experimental/src/resolve_call.rs
@@ -1,36 +1,37 @@
 use dilib::Container;
 
-pub trait ResolveCall {
+pub trait ResolveCall<Args> {
     type Output;
     fn resolve_call(&self, container: &Container) -> Self::Output;
 }
 
-pub trait ResolveCallMut {
+pub trait ResolveCallMut<Args> {
     type Output;
     fn resolve_call_mut(&mut self, container: &Container) -> Self::Output;
 }
 
-#[macro_export]
 macro_rules! impl_resolve_call_fn {
     ($($t:ident),+) => {
-        impl<Out, $($t),+> ResolveCall for Box<dyn Fn($(&$t),+) -> Out>
-         where $($t: Send + Sync + 'static),+ {
+        impl<Function, Out, $($t),+> ResolveCall<($($t),+,)> for Function
+         where Function: Fn($(&$t),+) -> Out,
+         $($t: Send + Sync + 'static),+ {
             type Output = Out;
             fn resolve_call(&self, container: &Container) -> Self::Output {
                 (self)(
-                    $(container.get::<$t>().unwrap().as_ref()),+
+                    $(container.get::<$t>().as_ref().unwrap()),+
                 )
             }
         }
     };
 
     (mut $($t:ident),+) => {
-        impl<Out, $($t),+> ResolveCallMut for Box<dyn FnMut($(&$t),+) -> Out>
-         where $($t: Send + Sync + 'static),+ {
+        impl<Function, Out, $($t),+> ResolveCallMut<($($t),+,)> for Function
+         where Function: FnMut($(&$t),+) -> Out,
+         $($t: Send + Sync + 'static),+ {
             type Output = Out;
             fn resolve_call_mut(&mut self, container: &Container) -> Self::Output {
                 (self)(
-                    $(container.get::<$t>().unwrap().as_ref()),+
+                    $(container.get::<$t>().as_ref().unwrap()),+
                 )
             }
         }
@@ -42,6 +43,7 @@ impl_resolve_call_fn!(A, B);
 impl_resolve_call_fn!(A, B, C);
 impl_resolve_call_fn!(A, B, C, D);
 impl_resolve_call_fn!(A, B, C, D, E);
+impl_resolve_call_fn!(A, B, C, D, E, F);
 impl_resolve_call_fn!(A, B, C, D, E, F, G);
 impl_resolve_call_fn!(A, B, C, D, E, F, G, H);
 impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I);
@@ -53,13 +55,19 @@ impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N);
 impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
 impl_resolve_call_fn!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
 
-// impl<Out, A> ResolveCall for Box<dyn Fn(&A) -> Out>
-// where
-//     A: Send + Sync + 'static,
-// {
-//     type Output = Out;
-//
-//     fn resolve_call(&self, container: &Container) -> Self::Output {
-//         (self)(container.get::<A>().unwrap().as_ref())
-//     }
-// }
+impl_resolve_call_fn!(mut A);
+impl_resolve_call_fn!(mut A, B);
+impl_resolve_call_fn!(mut A, B, C);
+impl_resolve_call_fn!(mut A, B, C, D);
+impl_resolve_call_fn!(mut A, B, C, D, E);
+impl_resolve_call_fn!(mut A, B, C, D, E, F);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K, L);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K, L, M);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K, L, M, N);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
+impl_resolve_call_fn!(mut A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
