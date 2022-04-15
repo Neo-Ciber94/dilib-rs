8d42bc20ea0e3a96d88a666408a8edff087a5d51
Freddy L <neo-ciber_94@hotmail.com>
2022-04-15 14:13:02 -0400

    Removed build.rs

--- dilib/build.rs
+++
@@ -1,9 +0,0 @@
-use std::{fs, path::PathBuf};
-
-fn main() {
-    println!("cargo:rerun-if-changed=README.md");
-    let file_path = fs::canonicalize("../README.md").unwrap();
-    let readme = fs::read_to_string(file_path.clone()).unwrap();
-    let output = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("README-lib.md");
-    fs::write(output, &readme).unwrap();
-}
