f028c721bce5a8cca17219cedc435033e4bccd2d
Freddy L <neo-ciber_94@hotmail.com>
2022-04-15 14:12:29 -0400

    Updated README.md for publish

--- dilib/build.rs
+++ dilib/build.rs
@@ -4,6 +4,6 @@ fn main() {
     println!("cargo:rerun-if-changed=README.md");
     let file_path = fs::canonicalize("../README.md").unwrap();
     let readme = fs::read_to_string(file_path.clone()).unwrap();
-    let output = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("README-lib.md");
+    let output = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("README-lib.md");
     fs::write(output, &readme).unwrap();
 }
