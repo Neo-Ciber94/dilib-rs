e502e09e1ba898d876692b89399a99ae50c4d40c
Freddy L <neo-ciber_94@hotmail.com>
2022-04-15 13:30:06 -0400

    Added build.rs

---
+++ dilib/build.rs
@@ -0,0 +1,8 @@
+use std::{fs, path::PathBuf};
+
+fn main() {
+    println!("cargo:rerun-if-changed=README.md");
+    let readme = fs::read_to_string("README.md").unwrap();
+    let output = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("README-lib.md");
+    fs::write(output, readme).unwrap();
+}
