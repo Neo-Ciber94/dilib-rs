e674fb62e1fcdf0ea6aa943e6405b90ee7db5923
Freddy L <neo-ciber_94@hotmail.com>
2022-04-15 14:03:41 -0400

    Updated build.rs

--- dilib/build.rs
+++ dilib/build.rs
@@ -2,7 +2,8 @@ use std::{fs, path::PathBuf};
 
 fn main() {
     println!("cargo:rerun-if-changed=README.md");
-    let readme = fs::read_to_string("../README.md").unwrap();
+    let file_path = fs::canonicalize("../README.md").unwrap();
+    let readme = fs::read_to_string(file_path.clone()).unwrap();
     let output = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("README-lib.md");
-    fs::write(output, readme).unwrap();
+    fs::write(output, &readme).unwrap();
 }
