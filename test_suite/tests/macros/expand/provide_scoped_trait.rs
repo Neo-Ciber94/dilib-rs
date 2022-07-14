4b154e850302b6ba293d5e6cce57c639a8e85616
Xiangru Lian <admin@mail.xrlian.com>
2022-07-14 18:52:51 +0000

    chore

--- test_suite/tests/macros/expand/provide_scoped_trait.rs
+++ test_suite/tests/macros/expand/provide_scoped_trait.rs
@@ -1,6 +1,11 @@
 use dilib::provide;
 
-#[provide(bind="std::ops::Add<usize>, std::ops::Mul<usize>")]
+#[provide(bind="std::ops::Add<usize>")]
 fn get_scoped_trait() -> usize {
+    123
+}
+
+#[provide(bind="std::ops::Add<usize>, std::ops::Mul<usize>")]
+fn get_multiple_scoped_trait() -> usize {
     123
 }
