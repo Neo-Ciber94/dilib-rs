2e203dd130812dde145a96f10fab614e28c1df0d
Freddy L <neo-ciber_94@hotmail.com>
2022-12-16 19:43:54 -0400

    feat: Updated sample

--- samples/local/src/main.rs
+++ samples/local/src/main.rs
@@ -1,3 +1,24 @@
+use dilib::{Container, Inject};
+
+#[derive(Default, Debug)]
+struct Engine;
+
+#[derive(Default, Debug, Clone, Copy)]
+struct Wheel;
+
+#[allow(dead_code)]
+#[derive(Debug, Inject)]
+struct Car {
+    engine: Engine,
+    wheel: [Wheel; 4],
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut container = Container::new();
+    container.add_scoped(Engine::default).unwrap();
+    container.add_scoped(|| [Wheel; 4]).unwrap();
+    container.add_deps::<Car>().unwrap();
+
+    let car = container.get::<Car>().unwrap();
+    println!("{:#?}", car);
 }
