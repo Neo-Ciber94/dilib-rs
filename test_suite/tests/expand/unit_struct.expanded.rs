55c43aec2c9fdfd53ca1f76fb7db370dfd021e10
Freddy L <neo-ciber_94@hotmail.com>
2022-12-17 21:20:08 -0400

    clean up expanded

--- test_suite/tests/expand/unit_struct.expanded.rs
+++
@@ -1,9 +0,0 @@
-use dilib::Inject;
-struct UnitStruct;
-impl dilib::Inject for UnitStruct {
-    #[allow(unused)]
-    #[allow(dead_code)]
-    fn inject(_: &dilib::Container) -> Self {
-        UnitStruct
-    }
-}
--- test_suite/tests/expand/unit_struct_brackets.expanded.rs
+++
@@ -1,9 +0,0 @@
-use dilib::Inject;
-struct UnitStructBrackets {}
-impl dilib::Inject for UnitStructBrackets {
-    #[allow(unused)]
-    #[allow(dead_code)]
-    fn inject(container: &dilib::Container) -> Self {
-        UnitStructBrackets {}
-    }
-}
